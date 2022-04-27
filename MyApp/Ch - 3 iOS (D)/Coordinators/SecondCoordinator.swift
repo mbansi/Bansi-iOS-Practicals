d7af01b7e6a8bb275b3a8bbd0020ee3701e575fd
Bansi Mamtora <mbansi306@gmail.com>
2022-04-27 10:59:18 +0530

    navigation completed

---
+++ MyApp/Ch - 3 iOS (D)/Coordinators/SecondCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  SecondCoordinator.swift
+//  MyApp
+//
+//  Created by Bansi Mamtora on 14/03/22.
+//
+
+
+import UIKit
+
+class SecondCoordinator: Coordinator {
+    
+    //MARK: - Variables
+    var navController: UINavigationController?
+    
+    //MARK: - Functions
+    init(_ navigationController: UINavigationController) {
+        navController = navigationController
+    }
+    
+    func start() { }
+    
+    func startWithData(_ message: String) {
+        if let home = UIStoryboard(name: Constants.navigationController, bundle: nil).instantiateViewController(withIdentifier: Constants.secondViewController) as? SecondViewController {
+            home.coordinator = self
+            home.message = message
+            navController?.pushViewController(home, animated: true)
+        }
+    }
+
+    func finish() {
+        navController?.popViewController(animated: true)
+    }
+    
+    func finishToRoot() {
+        navController?.popToRootViewController(animated: true)
+    }
+
+}
